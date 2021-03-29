f07d71461f78d731ef76db85164b118b89edd318
Sevkiscap <63850097+Sevkiscap@users.noreply.github.com>
2021-03-29 15:03:36 -0600

    Add files via upload

---
+++ Scripts/SceneLoader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int CurrenScenetIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(CurrenScenetIndex + 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
